3b5e8c2db9d27adbd1b51eec1b49a129eaef16ba
malapatisaiteja <malapatisaiteja@gmail.com>
2022-11-09 09:22:07 -0500

    Fixed swap request flow

--- src/Mechanic.java
+++ src/Mechanic.java
@@ -157,27 +157,27 @@ public class Mechanic {
                 int selfOrderId = 0;
                 int reqOrderId = 0;
                 DBConnection dbConn = DBConnection.getDBConnection();
-                String sql = "select ORDER_ID,  week, day, start_slot, end_slot from HOURLY_EMPLOYEE_SCHEDULE" +
-                        " where ORDER_ID<>-1 and EMPID=" + loginContext.ID + " and SCID=" + loginContext.SCID + " order by ORDER_ID";
+                String sql = "select ORDER_ID, WEEK, DAY, START_SLOT, END_SLOT from HOURLY_EMPLOYEE_SCHEDULE" +
+                        " where ORDER_ID!=-1 and EMPID=" + loginContext.ID + " and SCID=" + loginContext.SCID + " order by ORDER_ID";
                 Statement stmt = dbConn.conn.createStatement();
                 ResultSet rs = stmt.executeQuery(sql);
                 boolean selfSlot = false;
                 while (rs.next()) {
-                    if (rs.getInt("start_week") == week && rs.getInt("start_day") == day
-                            && rs.getInt("start_slot") == start_slot && rs.getInt("end_slot") == end_slot) {
+                    if (rs.getInt("WEEK") == week && rs.getInt("DAY") == day
+                            && rs.getInt("START_SLOT") == start_slot && rs.getInt("END_SLOT") == end_slot) {
                         selfSlot = true;
                         selfOrderId = rs.getInt("ORDER_ID");
                         break;
                     }
                 }
-                sql = "select ORDER_ID,  week, day, start_slot, end_slot from HOURLY_EMPLOYEE_SCHEDULE" +
+                sql = "select ORDER_ID, WEEK, DAY, START_SLOT, END_SLOT from HOURLY_EMPLOYEE_SCHEDULE" +
                         " where ORDER_ID<>-1 and EMPID=" + empID + " and SCID=" + loginContext.SCID + " order by ORDER_ID";
                 stmt = dbConn.conn.createStatement();
                 rs = stmt.executeQuery(sql);
                 boolean reqSlot = false;
                 while (rs.next()) {
-                    if (rs.getInt("start_week") == rweek && rs.getInt("start_day") == rday
-                            && rs.getInt("start_slot") == rstart_slot && rs.getInt("end_slot") == rend_slot) {
+                    if (rs.getInt("WEEK") == rweek && rs.getInt("DAY") == rday
+                            && rs.getInt("START_SLOT") == rstart_slot && rs.getInt("END_SLOT") == rend_slot) {
                         reqSlot = true;
                         reqOrderId = rs.getInt("ORDER_ID");
                         break;
