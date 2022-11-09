acb399404f11baebee26b552dca279be6c99c21a
malapatisaiteja <malapatisaiteja@gmail.com>
2022-11-08 21:56:26 -0500

    Repair scheduler changes

--- src/Cart.java
+++ src/Cart.java
@@ -3,19 +3,20 @@ import java.util.*;
 public class Cart {
     public String Maintainance;
     public int MaintenanceCost;
-    public ArrayList RepairServiceList;
+    public ArrayList<String> RepairServiceList;
+    public ArrayList<Integer> RepairServiceCostList;
 
     public Cart() {
         Maintainance = "";
         MaintenanceCost = 0;
-        RepairServiceList = new ArrayList<HashMap<String, Integer>>();
+        RepairServiceList = new ArrayList<String>();
+        RepairServiceCostList = new ArrayList<Integer>();
     }
 
     public int getTotalCost() {
         int sum = MaintenanceCost;
         for (int i=0;i<RepairServiceList.size();i++) {
-            HashMap<String, Integer> serviceObj = (HashMap) RepairServiceList.get(i);
-            sum += serviceObj.get("price");
+            sum += RepairServiceCostList.get(i);
         }
         return sum;
     }
